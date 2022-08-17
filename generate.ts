e18be3c109bb174698aabe04852370a64f4c3a2c
Sim Young Jae <hanul@hanul.me>
2022-08-17 13:49:23 +0900

    dev

--- generate.ts
+++ generate.ts
@@ -17,24 +17,40 @@ const generate = async (path: string, baseAttr: any, parts: any) => {
     const imageParts: any[] = [];
 
     for (const [traitId, trait] of parts.entries()) {
-
-        let totalPercent = 0;
-        let percentCount = 0;
-        for (const part of trait.parts) {
-            if ((part as any).percent !== undefined) {
-                totalPercent += (part as any).percent;
-                percentCount += 1;
+        if (
+            trait.condition === undefined ||
+            result.attributes.find((d: any) => d.trait_type === trait.condition.trait && trait.condition.values.includes(d.value) === true) !== undefined
+        ) {
+            let partsLength = 0;
+            let totalPercent = 0;
+            let percentCount = 0;
+            for (const part of trait.parts) {
+                if (
+                    (part as any).condition === undefined ||
+                    result.attributes.find((d: any) => d.trait_type === (part as any).condition.trait && (part as any).condition.values.includes(d.value) === true) !== undefined
+                ) {
+                    partsLength += 1;
+                    if ((part as any).percent !== undefined) {
+                        totalPercent += (part as any).percent;
+                        percentCount += 1;
+                    }
+                }
             }
-        }
-        const basePercent = (100 - totalPercent) / (trait.parts.length - percentCount);
+            const basePercent = (100 - totalPercent) / (partsLength - percentCount);
 
-        let rand = Math.random() * 100;
-        for (const [partId, part] of trait.parts.entries()) {
-            rand -= (part as any).percent === undefined ? basePercent : (part as any).percent;
-            if (rand <= 0) {
-                result.attributes.push({ trait_type: trait.name, value: part.name });
-                imageParts.push({ traitId, partId });
-                break;
+            let rand = Math.random() * 100;
+            for (const [partId, part] of trait.parts.entries()) {
+                if (
+                    (part as any).condition === undefined ||
+                    result.attributes.find((d: any) => d.trait_type === (part as any).condition.trait && (part as any).condition.values.includes(d.value) === true) !== undefined
+                ) {
+                    rand -= (part as any).percent === undefined ? basePercent : (part as any).percent;
+                    if (rand <= 0) {
+                        result.attributes.push({ trait_type: trait.name, value: part.name });
+                        imageParts.push({ traitId, partId });
+                        break;
+                    }
+                }
             }
         }
     }
@@ -61,25 +77,32 @@ const generate = async (path: string, baseAttr: any, parts: any) => {
             }
         }
 
-        await sharp({
-            create: {
-                width: 1024,
-                height: 1024,
-                channels: 4,
-                background: { r: 255, g: 167, b: 173, alpha: 0 }
-            }
-        })
-            .composite(parameters)
-            .png()
-            .toFile(`results/${id}.png`);
+        try {
+
+            await sharp({
+                create: {
+                    width: 1182,
+                    height: 1182,
+                    channels: 4,
+                    background: { r: 255, g: 167, b: 173, alpha: 0 }
+                }
+            })
+                .composite(parameters)
+                .png()
+                .toFile(`results/${id}.png`);
+
+        } catch (error) {
+            console.log(parameters);
+            throw error;
+        }
 
         console.log(`#${id} generated.`);
     }
 };
 
 (async () => {
-    await SkyUtil.repeatResultAsync(5000, async () => await generate("parts/", { trait_type: "Gender", value: "Man" }, boyParts));
-    await SkyUtil.repeatResultAsync(5000, async () => await generate("parts/", { trait_type: "Gender", value: "Woman" }, girlParts));
+    await SkyUtil.repeatResultAsync(972, async () => await generate("parts/", { trait_type: "Gender", value: "Boy" }, boyParts));
+    await SkyUtil.repeatResultAsync(973, async () => await generate("parts/", { trait_type: "Gender", value: "Girl" }, girlParts));
 
     for (let i = results.length - 1; i > 0; i--) {
         const j = Math.floor(Math.random() * (i + 1));
@@ -87,9 +110,9 @@ const generate = async (path: string, baseAttr: any, parts: any) => {
     }
 
     for (const [id, result] of results.entries()) {
-        await SkyFiles.write(`results-shuffle/${id}.png`, await SkyFiles.readBuffer(`results/${(result as any).id}.png`));
+        await SkyFiles.write(`images/${id}.png`, await SkyFiles.readBuffer(`results/${(result as any).id}.png`));
         (result as any).id = id;
+        (result as any).image = `https://storage.googleapis.com/webplusone/thekoreans/images/${id}.png`;
+        await SkyFiles.write(`metadata/${id}`, JSON.stringify(result));
     }
-
-    await SkyFiles.write("results.json", JSON.stringify(results));
 })();
